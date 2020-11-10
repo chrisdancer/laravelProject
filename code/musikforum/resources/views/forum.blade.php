306364476515758bc5ec903ef0c80ea0ada8255b
chrisdancer <c.daenzer@otterbach.de>
2020-11-10 19:39:18 +0100

    added list-view of forum

---
+++ code/musikforum/resources/views/forum.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Timestamp</th>
+                <th scope="col">User</th>
+                <th scope="col">Message</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($forumData as $key => $data)
+                <tr>
+                    <td>{{$data->created_at}}</td>
+                    <td>{{$data->userNr}}</td>
+                    <td>{{$data->message}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection

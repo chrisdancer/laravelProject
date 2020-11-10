efb1a2ff8d3043fc2693c1f98da06ce9c1583e43
chrisdancer <c.daenzer@otterbach.de>
2020-11-10 19:39:29 +0100

    added list-view of shows

---
+++ code/musikforum/resources/views/shows.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">Artist</th>
+            <th scope="col">Location</th>
+            <th scope="col">Tourname</th>
+            <th scope="col">Start</th>
+            <th scope="col">Date</th>
+            <th scope="col">Price</th>
+            <th scope="col">Safety Precautions</th>
+            <th scope="col">Cancelled</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach($showsData as $key => $data)
+                <tr>
+                    <td>{{$data->artistNr}}</td>
+                    <td>{{$data->locationNr}}</td>
+                    <td>{{$data->tourName}}</td>
+                    <td>{{$data->start}}</td>
+                    <td>{{$data->date}}</td>
+                    <td>{{$data->price}}</td>
+                    <td>{{$data->safetyPrecautions}}</td>
+                    <td>
+                        {{  $data->cancelled == 0 ? "no" : "yes" }}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection

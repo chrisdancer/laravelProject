f8005f2267dc1c550c4c9fc44ab058021d301174
chrisdancer <c.daenzer@otterbach.de>
2020-11-14 08:12:01 +0100

    tried gate but didnt work

--- code/musikforum/app/Providers/AuthServiceProvider.php
+++ code/musikforum/app/Providers/AuthServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Theme;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate as Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
     }
 }

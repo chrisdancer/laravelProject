7086cf427e6387a3135a4c1b4c4b8b85354abcab
chrisdancer <c.daenzer@otterbach.de>
2020-11-17 11:11:57 +0100

    added attributes

--- code/musikforum/app/Models/Carpool.php
+++ code/musikforum/app/Models/Carpool.php
@@ -4,16 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Carpool extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'driverName', 'departureLocation', 'destination', 'show'
+        'driverName', 'departureLocation', 'destination', 'show', 'departureTime', 'departureDate', 'seatsAvailable'
     ];
 
+    protected static function booted()
+    {
+        static::creating(function ($article) {
+            $article->user_id = Auth::id();
+        });
+    }
+
     public function user() {
-        Return $this->hasMany(User::class);
+        Return $this->belongsTo(User::class, 'user_id');
     }
 }
